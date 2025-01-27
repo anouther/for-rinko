3ab38074aa976236df86bd7a5ebe925def22f9d2
anouther <155570107+anouther@users.noreply.github.com>
2025-01-27 13:23:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Special Flower').split('');
+        const titles = ('RINKO').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
